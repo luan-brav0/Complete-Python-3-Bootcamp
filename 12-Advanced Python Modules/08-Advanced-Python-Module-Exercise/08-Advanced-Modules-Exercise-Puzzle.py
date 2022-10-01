9670c3ffbb17a5e0b0f6570c900f226077642fe1
luanbravo10 <luan.melo.bravo10@gmail.com>
2022-10-01 17:52:12 -0300

    advanced modules puzzle

--- 12-Advanced Python Modules/08-Advanced-Python-Module-Exercise/08-Advanced-Modules-Exercise-Puzzle.py
+++ 12-Advanced Python Modules/08-Advanced-Python-Module-Exercise/08-Advanced-Modules-Exercise-Puzzle.py
@@ -1,10 +1,35 @@
 '''Attempt of solution of the puzzle by luan_brav0'''
 
-from shutil import unpack_archive
+import shutil
 import zipfile
+import os
 
-unzip_path = "C:\\GitHub\\Complete-Python-3-Bootcamp\\12-Advanced Python Modules\\08-Advanced-Python-Module-Exercise\\unzip_me_for_instructions.zip"
+# unzip_path = "C:\\GitHub\\Complete-Python-3-Bootcamp\\12-Advanced Python Modules\\08-Advanced-Python-Module-Exercise"
 
-# unpack_archive('unzip_me_for_instructions.zip','','zip')
-with zipfile.ZipFile(unzip_path, 'r') as unziping:
-    unziping.extractall("C:\\GitHub\\Complete-Python-3-Bootcamp\\12-Advanced Python Modules\\08-Advanced-Python-Module-Exercise\\unzip_me_for_instructions")
+# shutil.unpack_archive('unzip_me_for_instructions.zip',unzip_path,'zip')
+# with zipfile.ZipFile(unzip_path, 'r') as unziped_file:
+    # unziping.extractall("C:\\GitHub\\Complete-Python-3-Bootcamp\\12-Advanced Python Modules\\08-Advanced-Python-Module-Exercise\\unzip_me_for_instructions")
+
+unzip_path = "C:\\Users\\luanm\\Documents\\GitHub\\Complete-Python-3-Bootcamp\\12-Advanced Python Modules\\08-Advanced-Python-Module-Exercise"
+os.chdir(unzip_path)
+print(os.getcwd())
+for folder, sub_folders, files in os.walk(unzip_path):
+    
+    print("Folder: "+ folder)
+    print('\n')
+    # print("THE SUBFOLDERS ARE: ")
+    for sub_fold in sub_folders:
+        print("\t Subfolder: "+sub_fold )
+        print('\n')
+    
+    
+    print("Files: ")
+    for f in files:
+        print("\t File: "+f)
+        print("Direcs: ", os.listdir())
+        with open(f,'r') as file:
+            print("Content: ", file.readline)  
+
+    print('\n')
+
+    
